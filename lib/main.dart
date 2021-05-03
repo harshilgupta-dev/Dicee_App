840be9500137d5afa5c35423fd0cdbd906d72817
Harshil Gupta <harshil.gupta_cs19@gla.ac.in>
2021-05-03 19:18:23 +0530

    add font and Image

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
 
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      backgroundColor: Colors.red,
+      backgroundColor: Colors.teal[400],
       appBar: AppBar(
-        title: Text('Dicee'),
-        backgroundColor: Colors.red,
+        title: Center(
+          child: Text('Dicee',
+            style: TextStyle(
+              fontFamily: 'Lobster',
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.2
+            ),
+          ),
+        ),
+        backgroundColor: Colors.amber[700],
       ),
       body: DicePage(),
     ),
@@ -25,35 +35,41 @@ class _DicePageState extends State<DicePage> {
   int rightDiceNumber=1;
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: FlatButton(
-              onPressed: (){
-                setState(() {
-                  leftDiceNumber=Random().nextInt(6)+1; // Random method genrate number between 0 to provided number(exclusive)
-                });
-              },
-              child: Image.asset(
-                  'images/dice$leftDiceNumber.png'
+    return Column(
+      children: [
+        Image.asset('images/gameOfDice.png'),
+        SizedBox(height: 50.0,),
+        Center(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: FlatButton(
+                  onPressed: (){
+                    setState(() {
+                      leftDiceNumber=Random().nextInt(6)+1; // Random method genrate number between 0 to provided number(exclusive)
+                    });
+                  },
+                  child: Image.asset(
+                      'images/dice$leftDiceNumber.png'
+                  ),
+                ),
               ),
-            ),
-          ),
-          Expanded(
-            child: FlatButton(
-              onPressed: (){
-                setState(() {
-                  rightDiceNumber=Random().nextInt(6)+1;
-                });
-              },
-              child: Image.asset(
-                  'images/dice$rightDiceNumber.png'
+              Expanded(
+                child: FlatButton(
+                  onPressed: (){
+                    setState(() {
+                      rightDiceNumber=Random().nextInt(6)+1;
+                    });
+                  },
+                  child: Image.asset(
+                      'images/dice$rightDiceNumber.png'
+                  ),
+                ),
               ),
-            ),
+            ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
   }
