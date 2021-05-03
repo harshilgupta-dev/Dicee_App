2f7b33706a2694d5b03fd5e9f6c1c0454ea8f825
Harshil Gupta <harshil.gupta_cs19@gla.ac.in>
2021-05-03 12:20:41 +0530

    add 16px padding to both images

--- lib/main.dart
+++ lib/main.dart
@@ -21,10 +21,16 @@ class DicePage extends StatelessWidget {
       child: Row(
         children: [
           Expanded(
-            child: Image.asset('images/dice1.png'),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('images/dice1.png'),
+            ),
           ),
           Expanded(
-            child: Image.asset('images/dice1.png'),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('images/dice1.png'),
+            ),
           )
         ],
       ),
